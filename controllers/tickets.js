6a829cb3192ec9988a7b4bf8c41fe20769c2206d
RazorHollow <rob@razorhollow.com>
2022-06-27 09:11:10 -0400

    add dashboard index page

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -1,5 +1,13 @@
-
+import { Profile } from '../models/profile.js'
+import random from 'random-key-generator'
 
 function index(req, res) {
-  
+  console.log()
+  res.render('tickets/index', {
+    title: "Dashboard",
+  })
+}
+
+export {
+  index
 }
