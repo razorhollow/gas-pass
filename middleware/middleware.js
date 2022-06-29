e10712ae436a151bfa510ef8713e9cf6acc6b6e9
RazorHollow <rob@razorhollow.com>
2022-06-28 22:20:37 -0400

    add isAdmin middleware

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,3 +1,5 @@
+import { Profile } from "../models/profile.js"
+
 function passUserToView(req, res, next) {
   res.locals.user = req.user ? req.user : null
   next()
@@ -8,7 +10,19 @@ function isLoggedIn(req, res, next) {
   res.redirect('/auth/google')
 }
 
+function isAdmin(req, res, next) {
+  Profile.findById(req.user.profile._id)
+  .then(profile => {
+    if(profile.isAdmin) {
+      return next()
+    }else {
+      res.redirect('/auth/google')
+    }
+  })
+}
+
 export {
   passUserToView,
-  isLoggedIn
+  isLoggedIn,
+  isAdmin
 }
--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,18 @@
 import { Router } from 'express'
 import { isLoggedIn } from '../middleware/middleware.js'
+import { isAdmin } from '../middleware/middleware.js'
 import * as adminCtrl from '../controllers/admin.js'
 
 const router = Router()
 
 // GET localhost:3000/admin
-router.get('/', isLoggedIn, adminCtrl.index)
+router.get('/', isLoggedIn, isAdmin, adminCtrl.index)
 
 //POST localhost:3000/admin
-router.post('/', adminCtrl.createAllowance)
+router.post('/', isAdmin, adminCtrl.createAllowance)
 
 //PUT LOCALHOST:3000/admin
-router.put('/', adminCtrl.updateAllowance)
+router.put('/', isAdmin, adminCtrl.updateAllowance)
 
 export {
   router
